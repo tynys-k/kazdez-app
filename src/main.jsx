2455248e026941a0288af2210aa578d65ccd2aa0
webdevfromkaskelen <webdevfromkaskelen@gmail.com>
2026-09-15 11:26:13 +0500

    Establish unified CRM design tokens and compact entity lists

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,8 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.css";
+import "./ui/design-tokens.css";
+import "./ui/primitives.css";
+import "./ui/legacy-bridge.css";
 
 createRoot(document.getElementById("root")).render(<App />);
